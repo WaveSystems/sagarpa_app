6bde7a6f3a9bd071744e7e8d6561d70059ce5eb9
Antonio Chavez <antonio.chavez@tangosource.com>
2012-01-05 18:31:31 -0600

    Clean application added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SagarpaApp::Application.config.secret_token = '410a59ac269b15ef7ff123a52495cf8bc2ce780d146afff1049ae1abe43c9dd41095619bb4d21ba79d6b594f94e17ad42b59c2f16b548eac3bc4bc5f139a1dad'
