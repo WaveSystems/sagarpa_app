75fe99d20ccfcff3df12b9096d33147d0a402990
Antonio Chavez <antonio.chavez@tangosource.com>
2012-01-09 14:48:52 -0600

    Fixed new permiso view with automated name of the creator

--- app/controllers/solicitantes_controller.rb
+++ app/controllers/solicitantes_controller.rb
@@ -5,9 +5,10 @@ class SolicitantesController < ApplicationController
   def new
     @permiso_pernocta = PermisoPernocta.new
     @id = PermisoPernocta.last.id+1
+    @solicita = User.find(current_user).nombre
   end
   def create
-    PermisoPernocta.create(params[:permiso_pernocta])
+    PermisoPernocta.create(params[:permiso_pernocta].merge(:solicita_id => current_user.id))
     redirect_to menu_solicitante_path
   end
   def search
@@ -25,5 +26,7 @@ class SolicitantesController < ApplicationController
     redirect_to menu_solicitante_path
   end
   def show_history
+    @user = current_user.id
+    @permiso_pernocta = PermisoPernocta.where("solicita_id = '#{@user}'") 
   end
 end
