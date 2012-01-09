498b92b46b7e3bc37cdf318a71dcd0fa4759153b
Antonio Chavez <antonio.chavez@tangosource.com>
2012-01-09 12:17:22 -0600

    Modified backend views of user

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register User do
-  
+  form do |f|
+    f.inputs "Usuario" do
+      f.input :nombre
+      f.input :email
+      f.input :password
+      f.input :area_id
+      f.input :autorizado, :as => :radio, :collection => User::AUTORIZADO
+      f.input :tipo, :as => :radio, :collection => User::TIPOS
+    end
+    f.buttons
+  end  
+  index do |f|
+    f.column :nombre
+    f.column :email
+    f.column :area_id
+    f.column :autorizado
+    f.column :tipo
+    default_actions
+  end
 end
