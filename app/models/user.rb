e5c9986746719a5380dd62357293e8d9990a3103
Antonio Chavez <antonio.chavez@tangosource.com>
2012-01-09 12:17:56 -0600

    Modified accesibility of user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
+  TIPOS = %w[normal administrador]
+  AUTORIZADO = %w[Si No]
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :nombre, :area_id, :autorizado, :tipo
 end
