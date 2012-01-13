92166c027cb0370257c889e8e1e9d19dba73e40e
Antonio Chavez <cavjzz@gmail.com>
2012-01-13 17:51:32 -0600

    Modified areas view on activemodel

--- app/admin/areas.rb
+++ app/admin/areas.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Area do
+  index do |f|
+    f.column :id
+    f.column :nombre
+    f.column :jefe_id
+    f.column :num_personal
+    f.column :num_vehiculos
+    default_actions
+  end
   
 end
