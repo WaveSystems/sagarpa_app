d9ed0d06c21a3940cbffcbe385c3646d4c2e6de2
Antonio Chavez + Jorge Robles <cavjzz@gmail.com>
2012-01-25 18:56:17 -0600

    Organized Permiso Diario index on activeadmin

--- app/admin/permiso_diarios.rb
+++ app/admin/permiso_diarios.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register PermisoDiario do
+
+  index do |f|
+    f.column :id
+    f.column :auto_id
+    f.column :justificacion
+    f.column :observaciones
+    f.column :fecha
+    f.column :hora_salida
+    f.column :hora_llegada
+    f.column :solicita_id
+    f.column :jefe_id
+  end
   
 end
