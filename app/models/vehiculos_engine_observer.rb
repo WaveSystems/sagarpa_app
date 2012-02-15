880ce13fe13295dc814c8b04a53795cd73dd4782
Antonio Chavez <cavjzz@gmail.com>
2012-02-14 18:44:39 -0600

    Added some validations to vehiculo

--- app/models/vehiculos_engine_observer.rb
+++ app/models/vehiculos_engine_observer.rb
@@ -1,4 +1,10 @@
 class VehiculosEngineObserver < ActiveRecord::Observer
   observe :permiso_pernocta
   observe :permiso_diario
+  
+  def after_save(permiso)
+    if  @vehiculo = Vehiculo.find(permiso.auto_id).estado == "Disponible" && permiso.estado == "Autorizado"
+      Vehiculo.estado = "No disponible"
+    end
+  end
 end
