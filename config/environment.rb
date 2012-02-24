03f144a359fec07de932df2b1cb9436d83532480
Antonio Chavez <cavjzz@gmail.com>
2012-02-23 23:31:25 -0600

    Fixed date and time formats

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SagarpaApp::Application.initialize!
+DateTime::DATE_FORMATS[:short]="short %m-%d-%Y %H:%M"
+Time::DATE_FORMATS[:short] = "%H:%M"
+Date::DATE_FORMATS[:short] = "short %Y-%m-%d"
+
+
