daf655b3a58476bd4eea509bd5d7872e892b3504
Antonio Chavez <cavjzz@gmail.com>
2012-02-24 08:36:31 -0600

    Changed print permiso views

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SagarpaApp::Application.initialize!
-DateTime::DATE_FORMATS[:short]="short %m-%d-%Y %H:%M"
+DateTime::DATE_FORMATS[:short]="%m-%d-%Y %H:%M"
 Time::DATE_FORMATS[:short] = "%H:%M"
 Date::DATE_FORMATS[:short] = "short %Y-%m-%d"
 
